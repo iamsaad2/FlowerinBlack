f182cf705b2aeffd7a79da0f8b22d322b8e327db
iamsaad2 <saadbadat.1@gmail.com>
2020-07-08 22:53:20 -0400

    deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -9,8 +10,6 @@ connectDB();
 //Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.json({ msg: 'Welcome to Notes' }));
-
 const PORT = process.env.local || 5000;
 
 //Define Routes
@@ -18,5 +17,16 @@ app.use('/api/profiles', require('./routes/profiles'));
 app.use('/api/flowers', require('./routes/flowers'));
 app.use('/api/managers', require('./routes/managers'));
 app.use('/api/auth', require('./routes/auth'));
+
+//Serve static assets in production
+
+if (process.env.NODE_ENV === 'production') {
+  //Set Static Folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 app.listen(PORT, () => console.log(`Server Started on Port ${PORT}`));
